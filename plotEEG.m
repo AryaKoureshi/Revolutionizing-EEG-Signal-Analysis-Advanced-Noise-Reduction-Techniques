91f50477178ddfd4b086c318c1229cd188242582
Arya <Arya.koureshi@gmail.com>
2023-12-21 20:33:20 +0330

    Delete plotEEG.m

--- plotEEG.m
+++
@@ -1,9 +0,0 @@
-% 32-channel data
-load('Electrodes') ;
-
-% Plot Data
-% Use function disp_eeg(X,offset,feq,ElecName)
-offset = max(abs(X(:))) ;
-feq = 200 ;
-% ElecName = Electrodes.labels ;
-disp_eeg(X,offset,feq,[]);
